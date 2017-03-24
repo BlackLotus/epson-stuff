5cf1725e1d1611b07d26687c23c2ba5d240cace5
Thomas Schneider <maxmusterm@gmail.com>
2017-03-24 20:27:01 +0100

    more logic for the dissector

--- enpc.lua
+++ enpc.lua
@@ -7,9 +7,10 @@ function enpc_proto.dissector(buffer,pinfo,tree)
     
 --    subtree = subtree:add(buffer(5,1),"Query or Command")
     if string.lower(buffer(5,1):string())==buffer(5,1):string() then
-        ctype="Answer"
+        ctype="Reply"
     else
-        ctype="Request"
+--  it's a request and doesn't have to be defined
+        ctype=""
     end
     if string.lower(buffer(5,1):string())=="q" then
         subtree:add(buffer(5,1),"Type: Query " .. ctype)
@@ -22,12 +23,50 @@ function enpc_proto.dissector(buffer,pinfo,tree)
     end
     subtree:add(buffer(6,1),"Device Type: " .. buffer(6,1):uint())
     subtree:add(buffer(7,1),"Device Number: " .. buffer(7,1):uint())
-    subtree:add(buffer(8,2),"Function Number: " .. buffer(8,2):uint())
-    subtree:add(buffer(10,2),"Result Code: " .. buffer(10,2):uint())
-    subtree:add(buffer(12,2),"Length: " .. buffer(12,2):uint())
+-- get function (maybe use a case instead)
+    freturn=buffer(8,2):uint()
+    if freturn==0 then
+        func="Basic Information"
+    elseif freturn==0x10 then
+        func="Status"
+    elseif freturn==0x11 then
+-- needs flow control and stuff
+        func="Forced Transmission"
+    elseif freturn==0x12 then
+        func="Reset"
+    elseif freturn==0x13 then
+        func="Buffer Flash"
+    elseif freturn==0x16 then
+        func="Clearing Connection Timeout Timer"
+    else
+        func="Unknown Function"
+    end
+
+    subtree:add(buffer(8,2),"Function Number: " .. buffer(8,2):uint() .. " " .. func)
+
+    if ctype=="" then
+        subtree:add(buffer(10,2),"(Fixed Value 0x0000) " .. buffer(10,2):uint())
+    else
+        if buffer(10,2):uint()==0 then
+            subtree:add(buffer(10,2),"Result Code: " .. buffer(10,2):uint() .. " (Normal end)")
+        elseif buffer(10,2):uint()==65534 then
+            subtree:add(buffer(10,2),"Result Code: " .. buffer(10,2):uint() .. " (No device requested)")
+        elseif buffer(10,2):uint()==65535 then
+            subtree:add(buffer(10,2),"Result Code: " .. buffer(10,2):uint() .. " (Function not supported)")
+        else
+            subtree:add(buffer(10,2),"Result Code: " .. buffer(10,2):uint() .. " (Unknown result code)")
+        end
+    end
+    subtree:add(buffer(12,2),"Command Length: " .. buffer(12,2):uint())
     length=buffer(12,2):int()
     if length > 0 then
-        subtree:add(buffer(14,length),"Command: " .. buffer(14,length):string())
+        if ctype=="" then
+            subtree = subtree:add(buffer(14,length), "Command")
+            subtree:add(buffer(14,length),"Foo: " .. buffer(14,length):string())
+         else
+            subtree = subtree:add(buffer(14,length), "Reply Data")
+            subtree:add(buffer(14,length),"Foo: " .. buffer(14,length):string())
+         end
     end
 end
 -- load the udp.port table
